fae25e40bcb86883d8aef253a0758c7593d0f176
kuba86 <8183159+kuba86@users.noreply.github.com>
2025-07-01 09:32:15 +0200

    Add ntfy.fish script for streamlined ntfy notifications via Fish shell

---
+++ files/ntfy.fish
@@ -0,0 +1,37 @@
+#! /usr/bin/env fish
+
+argparse 'title=' 'tags=' 'topic=' 'apikey=' 'priority=' 'message=' -- $argv
+or return
+
+if not set -ql _flag_title; \
+or not set -ql _flag_tags; \
+or not set -ql _flag_topic; \
+or not set -ql _flag_apikey
+    echo "'title', 'tags', 'topic', 'apikey' are required."
+    echo "'priority', 'message' are optional"
+    return 1
+end
+
+set -l encoded_auth_token (echo -n :"$_flag_apikey" | base64)
+
+if not set -ql _flag_priority
+    set -l _flag_priority "default"
+end
+
+if not set -ql _flag_message
+    set -l _flag_message ""
+end
+
+curl \
+--retry 5 \
+--connect-timeout 5 \
+--max-time 5 \
+--retry-delay 5 \
+--retry-all-errors \
+--fail \
+-H "Authorization: Basic $encoded_auth_token" \
+-H "Title: $_flag_title" \
+-H "Tags: $_flag_tags" \
+-H "Priority: $_flag_priority" \
+-d "$_flag_message" \
+"https://ntfy.kuba86.com/$_flag_topic"
