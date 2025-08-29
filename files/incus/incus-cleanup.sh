4020c79e0544821e90d7794bfea24459852f17c6
kuba86 <8183159+kuba86@users.noreply.github.com>
2025-08-29 10:46:58 +0200

    Add Incus cluster setup and management
    - Introduce playbooks: `incus-create-cluster.yaml` to initialize a new cluster and `incus-join-cluster.yaml` to join existing clusters.
    - Include `incus-preseed-create.yaml` and `incus-preseed-join.yaml` templates for automatic cluster configuration.
    - Update `incus-via-podman.yaml` for streamlined service management and enhanced storage setup.
    - Expand `incus-cleanup.sh` to reset and recreate cluster networks.
    - Adjust `incus.container` for better security by exposing `/sys/kernel/security`.

--- files/incus/incus-cleanup.sh
+++ files/incus/incus-cleanup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+sudo systemctl status incus.service
 sudo systemctl stop incus.service
-sudo btrfs property set /var/lib/incus/storage-pools/*/images/* ro false
+sudo btrfs property set /var/lib/incus/storage-pools/default/images/* ro false
 sudo rm -rf /var/lib/incus /var/lib/lxc /var/lib/lxcfs /var/lib/lxd
 sudo ip link delete incusbr0 type bridge
+
+
+sudo podman pull ghcr.io/cmspam/incus-docker:latest
 
 
 sudo mkdir /var/lib/incus
@@ -12,3 +16,17 @@ sleep 3
 sudo incus admin init --dump
 sudo incus admin init --auto --network-address=$TAILSCALE_IP --network-port=13512
 sudo incus admin init --dump
+
+
+# sudo systemctl stop incus.service
+# sudo btrfs property set /var/lib/incus/storage-pools/*/images/* ro false
+# sudo rm -rf /var/lib/incus /var/lib/lxc /var/lib/lxcfs /var/lib/lxd
+# sudo ip link delete incusbr0 type bridge
+
+
+sudo incus network create --target nuc02 incusnet0
+sudo incus network create --target wyse01 incusnet0
+sudo incus network create --target wyse02 incusnet0
+sudo incus network create --target wyse03 incusnet0
+sudo incus network create --target fedora-kde-xps incusnet0
+sudo incus network create incusnet0
