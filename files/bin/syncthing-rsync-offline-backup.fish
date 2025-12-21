ca4008cea43e76b22e43dd4d5f2c6d8467926209
kuba86 <8183159+kuba86@users.noreply.github.com>
2025-12-21 10:42:47 +0100

    Add `syncthing-rsync-offline-backup.fish` script for automated decryption, backup, and cleanup operations

---
+++ files/bin/syncthing-rsync-offline-backup.fish
@@ -0,0 +1,44 @@
+#! /usr/bin/env fish
+
+read -P "Proceed with decrypting dysk? (y/N): " -l confirm_decrypt
+if test "$confirm_decrypt" != "y"
+    echo "Decryption aborted."
+    exit 1
+end
+
+sudo cryptsetup open /dev/disk/by-id/usb-Samsung_SSD_860_QVO_2TB_012345678999-0:0 luks-2tb-ssd
+sudo mount -o compress=zstd:3 --source /dev/mapper/luks-2tb-ssd --target /var/mnt/2tb-ssd --onlyonce
+
+disk.info
+ll /var/mnt/2tb-ssd
+
+read -P "Proceed with BTRFS Scrub? (y/N): " -l confirm_scrub
+if test "$confirm_scrub" != "y"
+    echo "BTRFS Scrub aborted."
+    exit 1
+end
+
+sudo btrfs scrub start -B /var/mnt/2tb-ssd
+
+read -P "Proceed with rsync? (y/N): " -l confirm_rsync
+if test "$confirm_rsync" != "y"
+    echo "rsync aborted."
+    exit 1
+end
+
+rsync \
+  --archive \
+  --info=progress2,stats2 \
+  --human-readable \
+  --exclude='/syncthing/Kuba-media-download' \
+  /var/mnt/data1/syncthing /var/mnt/2tb-ssd/
+
+read -P "Proceed with umount? (y/N): " -l confirm_umount
+if test "$confirm_umount" != "y"
+    echo "umount aborted."
+    exit 1
+end
+
+sudo umount /var/mnt/2tb-ssd && sudo cryptsetup close luks-2tb-ssd
+
+echo "Backup process completed successfully."
