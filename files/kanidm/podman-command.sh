f54fd31e6c54e17762fbd7ccee93ae782e7ba89f
kuba86 <8183159+kuba86@users.noreply.github.com>
2026-01-03 15:22:46 +0100

    Set up restic backup for marek246 and wyse01 with snapshot pruning, Caddy configuration adjustments, and secrets update

--- files/kanidm/podman-command.sh
+++ files/kanidm/podman-command.sh
@@ -3,9 +3,8 @@
 podman run --rm -it --name kanidm-tools \
   --network=host \
   --workdir /root \
-  --volume=./files/kanidm/kanidm.config:/data/config:z \
   --volume=./files/kanidm/kanidm.config:/root/.config/kanidm:z \
-  --volume=./files/kanidm/kuba86.com.crt:/data/kuba86.com.crt:z \
+  --volume=/var/mnt/data/lets-encrypt/certs/kuba86.com.crt:/data/kuba86.com.crt:z,ro \
   --volume=./files/kanidm/.bash_history:/root/.bash_history:z \
   --volume=./files/kanidm/.bashrc:/root/.bashrc:z \
   --volume=./files/kanidm/provision_kanidm_oauth2.sh:/root/provision_kanidm_oauth2.sh:z \
