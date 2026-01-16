07a0f4280b710ad0ca162bd2c5d39b99632c2a08
kuba86 <8183159+kuba86@users.noreply.github.com>
2026-01-16 13:22:52 +0100

    Refactor syncthing backup script: replace exit conditions with `else` blocks for streamlined flow

--- files/bin/syncthing-rsync-offline-backup.fish
+++ files/bin/syncthing-rsync-offline-backup.fish
@@ -3,53 +3,49 @@
 read -P "Proceed with decrypting dysk? (y/N): " -l confirm_decrypt
 if test "$confirm_decrypt" != "y"
     echo "Decryption aborted."
-    exit 1
+else
+    sudo cryptsetup \
+        open \
+        /dev/disk/by-id/usb-Samsung_SSD_860_QVO_2TB_012345678999-0:0 \
+        luks-2tb-ssd && \
+    sudo mount \
+        -o compress=zstd:3 \
+        --source /dev/mapper/luks-2tb-ssd \
+        --target /var/mnt/2tb-ssd \
+        --onlyonce
 end
-
-sudo cryptsetup \
-    open \
-    /dev/disk/by-id/usb-Samsung_SSD_860_QVO_2TB_012345678999-0:0 \
-    luks-2tb-ssd && \
-sudo mount \
-    -o compress=zstd:3 \
-    --source /dev/mapper/luks-2tb-ssd \
-    --target /var/mnt/2tb-ssd \
-    --onlyonce
 
 disk.info
 
 read -P "Proceed with BTRFS Scrub? (y/N): " -l confirm_scrub
 if test "$confirm_scrub" != "y"
     echo "BTRFS Scrub aborted."
-    exit 1
+else
+    cmd-monitor \
+        --check-time=10 \
+        --check-command="sudo btrfs scrub status /var/mnt/2tb-ssd" \
+        --command-background="true" \
+        --command="sudo btrfs scrub start /var/mnt/2tb-ssd" \
+        --running-grep="status finished"
 end
-
-cmd-monitor \
-    --check-time=10 \
-    --check-command="sudo btrfs scrub status /var/mnt/2tb-ssd" \
-    --command-background="true" \
-    --command="sudo btrfs scrub start /var/mnt/2tb-ssd" \
-    --running-grep="status finished"
 
 read -P "Proceed with rsync? (y/N): " -l confirm_rsync
 if test "$confirm_rsync" != "y"
     echo "rsync aborted."
-    exit 1
+else
+    rsync \
+        --archive \
+        --info=progress2,stats2 \
+        --human-readable \
+        --exclude='/syncthing/Kuba-media-download' \
+        /var/mnt/data1/syncthing /var/mnt/2tb-ssd/
 end
-
-rsync \
-    --archive \
-    --info=progress2,stats2 \
-    --human-readable \
-    --exclude='/syncthing/Kuba-media-download' \
-    /var/mnt/data1/syncthing /var/mnt/2tb-ssd/
 
 read -P "Proceed with umount? (y/N): " -l confirm_umount
 if test "$confirm_umount" != "y"
     echo "umount aborted."
-    exit 1
+else
+    sudo umount /var/mnt/2tb-ssd && \
+    sudo cryptsetup close luks-2tb-ssd && \
+    echo "Backup process completed successfully."
 end
-
-sudo umount /var/mnt/2tb-ssd && \
-sudo cryptsetup close luks-2tb-ssd && \
-echo "Backup process completed successfully."
