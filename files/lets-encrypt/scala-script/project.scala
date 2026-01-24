69f3d236bd1b2fe4c78d121651e1cd89e14e1537
kuba86 <8183159+kuba86@users.noreply.github.com>
2026-01-24 19:34:27 +0100

    Update `os-lib` and `pprint` dependencies to latest versions

--- files/lets-encrypt/scala-script/project.scala
+++ files/lets-encrypt/scala-script/project.scala
@@ -12,8 +12,8 @@
 
 //> using dependency "com.github.alexarchambault::case-app:2.1.0"
 //> using dependency "com.github.pureconfig::pureconfig-core:0.17.9"
-//> using dependency "com.lihaoyi::os-lib:0.11.5"
-//> using dependency "com.lihaoyi::pprint:0.9.4"
+//> using dependency "com.lihaoyi::os-lib:0.11.6"
+//> using dependency "com.lihaoyi::pprint:0.9.6"
 //> using dependency "com.outr::scribe:3.17.0"
 //> using dependency "org.scala-lang.modules::scala-parallel-collections:1.2.0"
 //> using test.dependency "org.scalameta::munit:1.2.1"
