d9fdc460c6897fb2a505c08a870e5311716582ce
kuba86 <8183159+kuba86@users.noreply.github.com>
2026-02-07 13:30:20 +0100

    Refactor and simplify models: split `RenewOptions` into `RunOptions` and `RenewOptions`, clean up related logic, and update dependencies.

--- files/lets-encrypt/scala-script/src/main/scala/model/RenewOptions.scala
+++ files/lets-encrypt/scala-script/src/main/scala/model/RenewOptions.scala
@@ -1,35 +1,11 @@
 package com.kuba86.letsEntryptScript
 package model
+import caseapp.Recurse
 
 case class RenewOptions(
     logLevel: String = "info",
+    @Recurse(prefix = "cf-")
     cloudflare: Cloudflare,
-    certificate: Certificate,
+    @Recurse(prefix = "lego-")
     lego: Lego
 )
-
-object RenewOptions {
-  extension (options: RenewOptions)
-    def podmanCommand(domain: String, action: String): String = {
-      val noRandomSleep: String = if (action == "renew") "--no-random-sleep" else ""
-      s"""
-         |podman run --rm
-         |  --name=letsencrypt_lego_${domain}
-         |  --volume=${options.lego.letsEncryptPath}:/.lego:z
-         |  --env=CF_DNS_API_TOKEN=${options.cloudflare.cfApiToken}
-         |  --env=CLOUDFLARE_POLLING_INTERVAL=${options.cloudflare.cfPoolingInterval}
-         |  --env=CLOUDFLARE_PROPAGATION_TIMEOUT=${options.cloudflare.cfPropagationTimeout}
-         |  --env=CLOUDFLARE_TTL=${options.cloudflare.cfTtl}
-         |  --env=DNS_PROVIDER=${options.cloudflare.dnsProvider}
-         |  docker.io/goacme/lego:${options.lego.legoVersion}
-         |    --server=${options.lego.server}
-         |    --accept-tos
-         |    --dns.resolvers=${options.lego.dnsServers.mkString(",")}
-         |    --email=${options.lego.email}
-         |    --dns=${options.cloudflare.dnsProvider}
-         |    --domains=${domain}
-         |    --domains=*.${domain}
-         |    ${action} ${noRandomSleep}
-         |""".stripMargin
-    }
-}
