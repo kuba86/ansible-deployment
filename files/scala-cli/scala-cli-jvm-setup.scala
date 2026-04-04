8dab46b76e8c4898c55acfde73b56ddd6ad51dc5
kuba86 <8183159+kuba86@users.noreply.github.com>
2026-04-04 20:21:14 +0200

    Update Scala CLI playbook and JVM setup
    - Bumped Scala CLI version from `v1.12.2` to `v1.12.5` in the playbook.
    - Updated JVM version in `scala-cli-jvm-setup` script from `graalvm-community:21` to `graalvm-community:25`.
    - Extended playbook hosts to include `coreos`.

--- files/scala-cli/scala-cli-jvm-setup.scala
+++ files/scala-cli/scala-cli-jvm-setup.scala
@@ -1,8 +1,8 @@
-#!/usr/bin/env -S scala-cli shebang --server=false --jvm graalvm-community:21
+#!/usr/bin/env -S scala-cli shebang --server=false --jvm graalvm-community:25
 
 // check new versions: `cs java --available | grep graalvm-community`
 
 @main def main() = {
-  println("graalvm-community:21 downloaded")
+  println("graalvm-community:25 downloaded")
   println(s"Java home: ${sys.props("java.home")}")
 }
